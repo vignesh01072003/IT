69c96c9c664b8fed17fdca2cd6e6a7e12003789d
vignesh01072003 <133329344+vignesh01072003@users.noreply.github.com>
2023-08-24 11:42:57 +0530

    Add files via upload

---
+++ exchanging the value.py
@@ -0,0 +1,9 @@
+#to take input from the user
+x=input('enter value of x:')
+y=input('enter value of y:')
+#create a temporary variable and swap the value
+temp=x
+x=y
+y=temp
+print('the value of x after swapping:',x)
+print('the value of y after swapping:',y)
